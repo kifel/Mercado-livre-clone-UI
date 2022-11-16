242be1e0a22c3ef44f4f9d637f8a9a9c909842ad
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-16 00:03:09 -0300

    login page finalizada

--- src/pages/Login/Senha/index.tsx
+++ src/pages/Login/Senha/index.tsx
@@ -1,24 +1,144 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useContext } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { ScrollView, Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
-import { StackParams } from "../../../Routes/stack";
-
-// import { Container } from './styles';
+import { UserContext } from "../../../context/User";
+import { StackParams, User } from "../../../types/GlobalTypes";
+import {
+  Container,
+  Content,
+  Continue,
+  ContinueText,
+  Input,
+  InputContainer,
+  InputContainerButton,
+  PasswordText,
+  Placeholder,
+  Recover,
+  ReportContainer,
+  ReportDescription,
+  ReportTitle,
+  Texto,
+  Title,
+  TitleContainer
+} from "./styles";
 
 export const Senha: React.FC = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackParams>>();
+  const [passwordVisibility, setPasswordVisibility] = React.useState(true);
+  const [iconPassword, setIconPassword] = React.useState("EXIBIR");
+  const { user, LoginPass } = useContext<any>(UserContext);
+
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState,
+    formState: { errors },
+  } = useForm<User>({
+    defaultValues: {
+      login: "",
+    },
+  });
+
+  React.useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset({ login: "" });
+    }
+  }, [formState, reset]);
+
+  const handleSubmitForm = (data: User) => {
+    data.login = user;
+    console.log(data);
+  };
+
   const screenLogin = () => {
     navigation.navigate("Login");
   };
 
+  const handlePasswordVisibility = () => {
+    if (iconPassword === "EXIBIR") {
+      setIconPassword("OCULTAR");
+      setPasswordVisibility(!passwordVisibility);
+    } else if (iconPassword === "OCULTAR") {
+      setIconPassword("EXIBIR");
+      setPasswordVisibility(!passwordVisibility);
+    }
+  };
+
+  const changeAccount = () => {
+    LoginPass("");
+    screenLogin();
+  }
+
   return (
-    <View>
-      <TouchableOpacity onPress={screenLogin}>
-        <Icon name="arrow-left" color="black" size={24} />
-      </TouchableOpacity>
-      <Text>Pagina de Senha</Text>
-    </View>
+    <Container>
+      <ScrollView>
+        <Content>
+          <TitleContainer>
+            <TouchableOpacity onPress={screenLogin}>
+              <Icon name="arrow-left" color="black" size={24} />
+            </TouchableOpacity>
+            <Title>Agora, sua senha do Mercado Livre</Title>
+          </TitleContainer>
+          <InputContainer>
+            <Placeholder>Senha</Placeholder>
+            <InputContainerButton
+              style={{
+                border: `1px solid ${
+                  errors?.senha?.message ? "red" : " #c4c4c4"
+                }`,
+              }}
+            >
+              <Controller
+                control={control}
+                name="senha"
+                rules={{
+                  required: "senha não pode ser nula!!",
+                }}
+                render={({ field: { value, onChange } }) => (
+                  <>
+                    <Input
+                      value={value || ""}
+                      onChangeText={onChange}
+                      keyboardType="email-address"
+                      autoCapitalize="none"
+                      secureTextEntry={passwordVisibility}
+                    />
+                    <TouchableOpacity onPress={handlePasswordVisibility}>
+                      <Texto styles={{marginRight:'25px'}}>{iconPassword}</Texto>
+                    </TouchableOpacity>
+                  </>
+                )}
+              />
+            </InputContainerButton>
+            {errors?.senha && (
+              <Text style={{ color: "red" }}>{errors.senha.message}</Text>
+            )}
+          </InputContainer>
+          <Continue onPress={handleSubmit(handleSubmitForm)}>
+            <ContinueText>Continuar</ContinueText>
+          </Continue>
+          <Recover>
+            <PasswordText>Não sei a minha senha</PasswordText>
+          </Recover>
+          <Recover onPress={changeAccount}>
+            <Texto>Não sou {user}</Texto>
+          </Recover>
+          <ReportContainer>
+            <TouchableOpacity>
+              <ReportTitle>Reportar um problema</ReportTitle>
+              <ReportDescription>
+                <Icon name="smartphone" color="black" size={24} />
+                {"  "}Roubo ou perda do Telefone{"    "}
+                <Icon name="chevron-right" color="black" size={24} />
+              </ReportDescription>
+            </TouchableOpacity>
+          </ReportContainer>
+        </Content>
+      </ScrollView>
+    </Container>
   );
 };
---
+++ src/pages/Login/Senha/styles.js
@@ -0,0 +1,92 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #ffff;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  width: 100%;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const ReportContainer = styled.View`
+  width: 100%;
+  border: 1px solid #c4c4c4;
+  padding: 25px;
+  margin-top: 50px;
+  border-radius: 8px;
+`;
+
+export const InputContainerButton = styled.View`
+  width: 100%;
+  border-radius: 8;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ReportTitle = styled.Text`
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+
+export const PasswordText = styled.Text`
+  margin-bottom: 40px;
+  color: #3483fa;
+`;
+
+export const ReportDescription = styled.Text`
+  font-size: 16px;
+`;
+
+export const TitleContainer = styled.View`
+  width: 100%;
+  margin-top: 25px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  margin-top: 40px;
+  font-weight: bold;
+`;
+
+export const InputContainer = styled.View`
+  width: 100%;
+  margin-bottom: 40px;
+`;
+
+export const Input = styled.TextInput`
+  width: 80%;
+  padding: 8px;
+  border-radius: 8px;
+`;
+
+export const Texto = styled.Text`
+  color: #3483fa;
+  font-weight: 600;
+`;
+
+export const Recover = styled.TouchableOpacity`
+  margin-top: 25px;
+`;
+
+export const Continue = styled.TouchableOpacity`
+  width: 100%;
+  padding: 15px;
+  border-radius: 8px;
+  background-color: #3483fa;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContinueText = styled.Text`
+  color: #fff;
+`;
+
+export const Placeholder = styled.Text`
+  margin-top: 80px;
+  font-size: 11px;
+  margin-bottom: 5px;
+`;
