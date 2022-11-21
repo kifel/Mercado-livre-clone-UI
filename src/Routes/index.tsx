68919ba04713a008a5eabe74582931d71873a043
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-21 10:19:33 -0300

    fix styles

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,8 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { StackRoutes } from "./stack";
 
-// import { Container } from './styles';
-
 // [x]Rotas configuradas
 export const RoutesConfig: React.FC = () => {
   return (
